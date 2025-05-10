4d1e7089db613cd6d35e079257b2acc36e406b79
Will Sobel <will@wvsobel.llc>
2025-05-09 19:49:53 -0700

    Added some simple plant uml to visualize the graph

--- mtc2rdf.rb
+++ mtc2rdf.rb
@@ -14,10 +14,11 @@ require 'Designators'
 require 'example'
 require 'rdf/rdfxml'
 require 'rdf/turtle'
+require 'set'
 
 include RDF
 
-machine = ARGV[0] 
+machine = ARGV[0]
 path = "/#{machine}/probe"
 puts "Getting https://demo.mtconnect.org#{path}"
 
@@ -47,7 +48,7 @@ def add_capability(graph, iri, names, type)
   if cls = Example::Functions[type.to_sym]
     add_instance(graph, iri, sub_iri(names, 'function'), IOF::Core.hasFunction, cls)
   end
-  if cls = Example::Capabilities[type.to_sym]    
+  if cls = Example::Capabilities[type.to_sym]
     add_instance(graph, iri, sub_iri(names, 'capability'), IOF::Core.hasCapability, cls)
   end
 end
@@ -64,8 +65,8 @@ def add_component(graph, comp, names = [], level = 0)
     iri = Inst::Data[names.join('-')]
     graph << [iri, RDF.type, cls]
     graph << [parent, BFO::BFO.has_member_part_at_some_time, iri] if parent
-    
-    s = add_instance(graph, iri, sub_iri(names, "name"), OMG::Designators.hasName, OMG::Designators.Name)    
+
+    s = add_instance(graph, iri, sub_iri(names, "name"), OMG::Designators.hasName, OMG::Designators.Name)
     graph << [s.subject, IOF::Core.hasSimpleExpressionValue, "#{comp['name'] || comp['id']}"]
 
     specs = comp.get_elements('./Configuration/Specifications/*')
@@ -76,7 +77,7 @@ def add_component(graph, comp, names = [], level = 0)
         spec.each_element do |des|
           puts "#{'  ' * (level + 2)}-> #{des.name}: #{des.text}"
           sn = ["spec", spec[:type], spec[:subType], des.name].compact
-          v = add_instance(graph, sp.subject, sub_iri(names, sn), IOF::Core.hasValueExpressionAtAllTimes, IOF::Core.ValueExpression)        
+          v = add_instance(graph, sp.subject, sub_iri(names, sn), IOF::Core.hasValueExpressionAtAllTimes, IOF::Core.ValueExpression)
           graph << [v.subject, IOF::Core.hasSimpleExpressionValue, des.text.to_f]
           graph << [v.subject, QUDT.hasUnit, Units[spec[:units].to_sym]]
         end
@@ -99,7 +100,7 @@ def add_component(graph, comp, names = [], level = 0)
       if cls = Example::DataItems[di[:type].to_sym]
         name = [di[:type], di[:subType]].compact.join('-').downcase
         puts "#{'  ' * (level + 2)}** adding #{name}"
-        add_instance(graph, iri, sub_iri(names, name), IOF::Core.measuresAtSomeTime, cls)        
+        add_instance(graph, iri, sub_iri(names, name), IOF::Core.measuresAtSomeTime, cls)
       end
     end
   end
@@ -109,7 +110,7 @@ def add_component(graph, comp, names = [], level = 0)
   end
 
   comp.each_element("./Compositions/*") do |cmp|
-    add_component(graph, cmp, names, level + 1)    
+    add_component(graph, cmp, names, level + 1)
   end
 
   comp.each_element('./Components/*') do |child|
@@ -121,6 +122,77 @@ doc = REXML::Document.new(resp.body)
 
 doc.each_element('//Device') do |dev|
   add_component(graph, dev)
+end
+
+def uml_name(iri)
+  qn = iri.qname(prefixes: Prefixes)
+  if qn[0] == :obo
+    "obo:#{iri.attributes[:label][:en]}"
+  else
+    qn.join(':')
+  end
+end
+
+def add_object(objects, iri)
+  name = uml_name(iri)
+  unless objects[name]
+    objects[name] = objects.size + 1
+  end
+end
+
+def add_type(types, iri, type)
+  types[uml_name(iri)] = uml_name(type)
+end
+
+
+File.open("#{machine}.puml", 'w') do |f|
+  f.puts "@startuml"
+  f.puts "skinparam linetype polyline"
+  f.puts "left to right direction"
+  objects = Hash.new
+  types = Hash.new
+  graph.each_statement do |stmt|
+    unless uml_name(stmt.predicate) == 'rdfv:type'      
+      add_object(objects, stmt.subject)
+      unless RDF::Literal === stmt.object
+        add_object(objects, stmt.object)
+      end
+    else
+      add_type(types, stmt.subject, stmt.object)
+    end
+    
+  end
+
+  objects.each do |k, v|
+    f.print "object \"#{k}\" as o#{v}"
+    if types[k]
+      f.puts "{\n type = #{types[k]}\n }"
+    else
+      f.puts
+    end
+  end
+
+  graph.each_statement do |stmt|
+    unless uml_name(stmt.predicate) == 'rdfv:type'      
+      if RDF::Literal === stmt.object
+        f.puts "o#{objects[uml_name(stmt.subject)]} : #{uml_name(stmt.predicate)} = #{stmt.object.value}"
+      else
+        f.puts "o#{objects[uml_name(stmt.subject)]} --> o#{objects[uml_name(stmt.object)]} : #{uml_name(stmt.predicate)}"
+      end
+    end
+  end
+
+#    f.puts "object #{stmt.subject.qname(prefixes: Prefixes).join(':')}"
+#    if RDF::Literal === stmt.object
+#      p stmt.object.value
+#    else
+#      p stmt.object.qname(prefixes: Prefixes).join(':')
+#    end
+#    p stmt.subject.qname(prefixes: Prefixes).join(':')
+#    p stmt.predicate.qname(prefixes: Prefixes).join(':')
+#    puts '======'
+#  end
+  f.puts("@enduml")
 end
 
 RDF::Writer.open("#{machine}.rdf", prefixes: Prefixes) do |w|
