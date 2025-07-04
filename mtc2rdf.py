2de842a0687da3b90aa3d89fb66bd39ea9e34106
Will Sobel <will@wvsobel.llc>
2025-07-04 16:09:39 +0200

    Added part diagrams

--- mtc2rdf.py
+++ mtc2rdf.py
@@ -6,6 +6,12 @@ from mtconnect_to_iof import MTConnectToIOF
 from generate_diagram import GenerateDiagram
 from owlready2.dl_render import dl_render_terminology_str, dl_render_class_str
 import re
+from indented_logger import setup_logging, increase_indent, decrease_indent
+from indented_logger.decorator import log_indent
+import logging
+
+setup_logging(level = logging.DEBUG, indent_spaces=2, include_func=True, no_datetime=True)
+logger = logging.getLogger(__name__)
 
 Example.load(only_local=True)
 
@@ -31,10 +37,29 @@ entities = [x[0] for x in owl.default_world.sparql("""select ?p { ?p a ?c . ?c r
 
 GenerateDiagram.linked_terms = set(entities)
 
-data = list(owl.default_world.sparql(f"""select ?s ?p ?o {{ ?s ?p ?o . FILTER(LIKE(STR(?s), "{Data.base_iri}%")) }}"""))
+all_data = list(owl.default_world.sparql(f"""select ?s ?p ?o {{ ?s ?p ?o . FILTER(LIKE(STR(?s), "{Data.base_iri}%")) }}"""))
+full = GenerateDiagram(f"{Vendor.name}Full", all_data, Vendor.name)
+logger.info("Generating full diagram")
+full.generate()
 
-gen = GenerateDiagram(f"{Vendor.name}Full", data, Vendor.name)
-gen.generate()
+for part in entities:
+  logger.info(f"Generating diagram for {part}")  
+  raw = owl.default_world.sparql("""select distinct ?p ?o ?p2 ?o2 { ?? ?p ?o . ?o ?p2 ?o2 . }""", [part])
+  statements = []
+  for x in raw:
+    l1 = [part, x[0], x[1]]
+    if l1 not in statements:
+      statements.append(l1)
+    if x[1].namespace != Vendor:
+      l2 = [x[1], x[2], x[3]]
+      if l2 not in statements:
+        statements.append(l2)
+  pre = owl.default_world.sparql("select distinct ?s ?p { ?s ?p ?? .}", [part])
+  for x in pre:
+    statements.append([x[0], x[1], part])
+  gen = GenerateDiagram(f"{part.name}", statements, part.name)
+  gen.generate()
+  
 
 with open("index.html", 'w') as f:
     f.write(f"""
